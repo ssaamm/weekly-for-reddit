dead7417abc44af6d7d01c66a2614c26deebaf2d
Samuel Taylor <git@samueltaylor.org>
2019-12-23 08:38:47 -0800

    Add URL parameter for limit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import datetime as dt
 
 import praw
 import PyRSS2Gen as rss
-from flask import Flask
+from flask import Flask, request
 from markdown import markdown
 
 from secrets import secret_params
@@ -14,14 +14,16 @@ reddit.read_only = True
 
 def get_top(subreddit, r, limit):
     sub = r.subreddit(subreddit)
-    return sub.top('week', limit=limit)
+    return sub.top("week", limit=limit)
+
 
 def render_description(selftext, link):
     if selftext:
         return markdown(selftext)
-    if any(link.endswith(suffix) for suffix in ('jpg', 'png', 'gif')):
+    if any(link.endswith(suffix) for suffix in ("jpg", "png", "gif")):
         return f'<img src="{link}">'
     return f'<a href="{link}">Link</a>'
+
 
 def post_to_rss(post):
     return rss.RSSItem(
@@ -36,22 +38,22 @@ def post_to_rss(post):
 
 def top_rss(subreddit, r, limit=10):
     return rss.RSS2(
-        title=f'Top /r/{subreddit} posts',
-        link=f'https://reddit.com/r/{subreddit}/top',
-        description=f'Top posts from /r/{subreddit}',
+        title=f"Top /r/{subreddit} posts",
+        link=f"https://reddit.com/r/{subreddit}/top",
+        description=f"Top {limit} posts from /r/{subreddit}",
         lastBuildDate=dt.datetime.utcnow(),
-        items=[
-            post_to_rss(post) for post in
-            get_top(subreddit, reddit, limit)
-        ]
+        items=[post_to_rss(post) for post in get_top(subreddit, reddit, limit)],
     )
 
 
-@app.route('/sub/<name>')
+@app.route("/sub/<name>")
 def create_feed(name):
-    limit = 10
+    try:
+        limit = int(request.args.get("limit", "10"))
+    except ValueError:
+        limit = 10
     return top_rss(name, reddit, limit).to_xml()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
